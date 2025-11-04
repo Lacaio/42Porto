691ebe8a7ddd50d3ab63bb4a27aa1af7b805f0d2
Lucas Prado Lopes Rebelo <lprado-l@c1r5s2.42porto.com>
2025-11-04 12:50:12 +0000

    Started ft_strtrim and removed exec_prog2

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,11 +6,36 @@
 /*   By: lprado-l <lprado-l@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 01:23:18 by lprado-l          #+#    #+#             */
-/*   Updated: 2025/11/04 01:24:51 by lprado-l         ###   ########.fr       */
+/*   Updated: 2025/11/04 12:40:34 by lprado-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char *ft_strtrim(char const *s1, char const *set)
 {
+int	i;
+int	j;
+int	k;
+int	len_s1;
+int	len_set;
+char	*str;
 
+i = 0;
+j = 0;
+len_set = 0;
+len_s1 = 0;
+while (set[len_set] != '\0')
+	len_set++;
+while (s1[len_s1] != '\0')
+	len_s1++;
+while (s1[i] == set[j] && s1[i] != '\0')
+{
+	j++;
+	if (set[j] == '\0')
+		j = 0;
+	i++;
+
+}
+while (
 }
